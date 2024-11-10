348e7febd493bd17a8ce989f134a5e1fba2291d0
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-09 22:41:43 -0600

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -7,3 +7,5 @@ import time
 # Configuramos el cliente
 server_ip = "172.168.2.25"  # Cambia a la IP del servidor
 server_port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
